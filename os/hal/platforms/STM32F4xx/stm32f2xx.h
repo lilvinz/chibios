0722b3aa8c0473b275b2bdfe2f470b59028b23e0
vke <vincent.kessler@quantec-networks.de>
2014-01-23 20:33:57 +0100

    ChibiOS: STM32F4xx fixed HSE_VALUE in stm32f2xx.h

--- os/hal/platforms/STM32F4xx/stm32f2xx.h
+++ os/hal/platforms/STM32F4xx/stm32f2xx.h
@@ -87,7 +87,9 @@
    Tip: To avoid modifying this file each time you need to use different HSE, you
         can define the HSE value in your toolchain compiler preprocessor.
   */           
-#define HSE_VALUE            ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
+#if !defined  (HSE_VALUE)
+  #define HSE_VALUE            ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
+#endif /* HSE_VALUE */
 
 /**
  * @brief In the following line adjust the External High Speed oscillator (HSE) Startup 
