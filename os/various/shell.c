bb25be6182d2ebf1740a05e1373badf46f1868f0
vke <vincent.kessler@quantec-networks.de>
2014-03-21 23:57:32 +0100

    ChibiOS: added simulator support to shell.c

--- os/various/shell.c
+++ os/various/shell.c
@@ -293,7 +293,12 @@ bool_t shellGetLine(BaseSequentialStream *chp, char *line, unsigned size) {
       }
       continue;
     }
-    if (c == '\r') {
+    if (c ==
+#if defined(CH_ARCHITECTURE_SIMIA32)
+        '\n') {
+#else
+        '\r') {
+#endif
       chprintf(chp, "\r\n");
       *p = 0;
       return FALSE;
