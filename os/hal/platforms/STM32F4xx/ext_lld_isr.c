b326851f5366c2f75d3d3a7d4d07a455b7f6c567
vke <vincent.kessler@quantec-networks.de>
2014-05-15 22:55:20 +0200

    ChibiOS: disable exti line 19 for STM32F401

--- os/hal/platforms/STM32F4xx/ext_lld_isr.c
+++ os/hal/platforms/STM32F4xx/ext_lld_isr.c
@@ -224,6 +224,7 @@ CH_IRQ_HANDLER(OTG_FS_WKUP_IRQHandler) {
   CH_IRQ_EPILOGUE();
 }
 
+#if !defined(STM32F401xx)
 /**
  * @brief   EXTI[19] interrupt handler (ETH_WKUP).
  *
@@ -239,7 +240,6 @@ CH_IRQ_HANDLER(ETH_WKUP_IRQHandler) {
   CH_IRQ_EPILOGUE();
 }
 
-#if !defined(STM32F401xx)
 /**
  * @brief   EXTI[20] interrupt handler (OTG_HS_WKUP).
  *
@@ -317,9 +317,9 @@ void ext_lld_exti_irq_enable(void) {
                    CORTEX_PRIORITY_MASK(STM32_EXT_EXTI17_IRQ_PRIORITY));
   nvicEnableVector(OTG_FS_WKUP_IRQn,
                    CORTEX_PRIORITY_MASK(STM32_EXT_EXTI18_IRQ_PRIORITY));
+#if !defined(STM32F401xx)
   nvicEnableVector(ETH_WKUP_IRQn,
                    CORTEX_PRIORITY_MASK(STM32_EXT_EXTI19_IRQ_PRIORITY));
-#if !defined(STM32F401xx)
   nvicEnableVector(OTG_HS_WKUP_IRQn,
                    CORTEX_PRIORITY_MASK(STM32_EXT_EXTI20_IRQ_PRIORITY));
   nvicEnableVector(TAMP_STAMP_IRQn,
@@ -346,8 +346,8 @@ void ext_lld_exti_irq_disable(void) {
   nvicDisableVector(PVD_IRQn);
   nvicDisableVector(RTC_Alarm_IRQn);
   nvicDisableVector(OTG_FS_WKUP_IRQn);
-  nvicDisableVector(ETH_WKUP_IRQn);
 #if !defined(STM32F401xx)
+  nvicDisableVector(ETH_WKUP_IRQn);
   nvicDisableVector(OTG_HS_WKUP_IRQn);
   nvicDisableVector(TAMP_STAMP_IRQn);
 #endif /* !defined(STM32F401xx) */
