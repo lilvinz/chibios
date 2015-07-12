f6409b8d7f7138cfbc102f51344f6440f6503702
vke <vincent.kessler@quantec-networks.de>
2015-07-12 13:35:02 +0200

    os/various/syscalls: add empty _tzset_unlocked_r to prevent heap The issue was introduced with gcc arm embedded 2015q2.

--- os/various/syscalls.c
+++ os/various/syscalls.c
@@ -172,6 +172,13 @@ int _isatty_r(struct _reent *r, int fd)
   return 1;
 }
 
+/***************************************************************************/
+
+void _tzset_unlocked_r(struct _reent *r)
+{
+  (void)r;
+}
+
 #endif /* PORT_ARCHITECTURE_SIMIA32 */
 
 /*** EOF ***/
