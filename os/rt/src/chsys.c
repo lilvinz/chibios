7e5e284350335d99834eb3b4996132751890c856
vke <vincent.kessler@quantec-networks.de>
2016-01-10 20:04:20 +0100

    rt/chsys.c: set chSysInit() to used to enable thread aware debugging when using -flto

--- os/rt/src/chsys.c
+++ os/rt/src/chsys.c
@@ -90,6 +90,7 @@ static void _idle_thread(void *p) {
  *
  * @special
  */
+__attribute__((used))
 void chSysInit(void) {
 #if CH_DBG_ENABLE_STACK_CHECK == TRUE
   extern stkalign_t __main_thread_stack_base__;
