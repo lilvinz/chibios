5ff49a113f1fe9be377023e2a1327748c3c43122
vke <vincent.kessler@quantec-networks.de>
2016-01-28 23:33:50 +0100

    community/STM32F4xx/platform.mk: remove non-existing source file reference

--- community/os/hal/ports/STM32/STM32F4xx/platform.mk
+++ community/os/hal/ports/STM32/STM32F4xx/platform.mk
@@ -5,8 +5,7 @@ PLATFORMSRC += ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/DMA2Dv1/stm32_dma2d.c \
                ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1/nand_lld.c \
                ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1/fsmc_sram.c \
                ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/LTDCv1/stm32_ltdc.c \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/TIMv1/eicu_lld.c \
-               ${CHIBIOS_CONTRIB}/os/hal/src/fsmc_sdram.c
+               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/TIMv1/eicu_lld.c
 
 PLATFORMINC += ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/DMA2Dv1 \
                ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1 \
