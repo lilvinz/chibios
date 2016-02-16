8b1f2263f7aec0c21f436ff6ec5f0258e71c2904
vke <vincent.kessler@quantec-networks.de>
2016-02-16 15:47:48 +0100

    community/hal: add support for STM32L4xx

--- community/os/hal/ports/STM32/LLD/FSMCv1/hal_fsmc.h
+++ community/os/hal/ports/STM32/LLD/FSMCv1/hal_fsmc.h
@@ -36,6 +36,9 @@
  */
 #if (defined(STM32F427xx) || defined(STM32F437xx) || \
      defined(STM32F429xx) || defined(STM32F439xx) || \
+     defined(STM32L471xx) || defined(STM32L475xx) || \
+     defined(STM32L476xx) || defined(STM32L485xx) || \
+     defined(STM32L486xx) || defined(STM32L496xx) || \
      defined(STM32F745xx) || defined(STM32F746xx) || \
      defined(STM32F756xx) || defined(STM32F767xx) || \
      defined(STM32F769xx) || defined(STM32F777xx) || \
