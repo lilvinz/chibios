f8e5d1f34ae639c8cdbc7416b1c11b80da651041
lilvinz <vincent.kessler@quantec-group.com>
2018-07-01 13:23:43 +0200

    os/hal/ports/STM32/STM32L4xx/stm32_rcc.h: add I2C4

--- os/hal/ports/STM32/STM32L4xx/stm32_rcc.h
+++ os/hal/ports/STM32/STM32L4xx/stm32_rcc.h
@@ -546,6 +546,30 @@
 /** @} */
 
 /**
+ * @brief   Enables the I2C4 peripheral clock.
+ *
+ * @param[in] lp        low power enable flag
+ *
+ * @api
+ */
+#define rccEnableI2C4(lp) rccEnableAPB1R2(RCC_APB1ENR2_I2C4EN, lp)
+
+/**
+ * @brief   Disables the I2C4 peripheral clock.
+ *
+ * @api
+ */
+#define rccDisableI2C4() rccDisableAPB1R2(RCC_APB1ENR2_I2C4EN)
+
+/**
+ * @brief   Resets the I2C4 peripheral.
+ *
+ * @api
+ */
+#define rccResetI2C4() rccResetAPB1R2(RCC_APB1RSTR2_I2C4RST)
+/** @} */
+
+/**
  * @name    OTG peripherals specific RCC operations
  * @{
  */
